02e44206fa0916b7e48a8085ddb1c2842fa3fbfe
izharj <eezhar@gmail.com>
2023-04-09 03:06:56 +0800

    Update ApplicationConfig.java

--- springjwt/src/main/java/com/izhar/springjwt/config/ApplicationConfig.java
+++ springjwt/src/main/java/com/izhar/springjwt/config/ApplicationConfig.java
@@ -4,8 +4,14 @@ import com.izhar.springjwt.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,5 +24,29 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService(){
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
+    }
+
+    // DAO responsible for fetching user details and encode password and etc...
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        // specify the following properties:
+        // specify user details from where
+        authProvider.setUserDetailsService(userDetailsService());
+        // password encoder
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    // this bean is used to authenticate user's credentials
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    // for encrypting user password
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
